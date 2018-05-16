690adb3451c5507943084bca8662aa9c73b1d6f7
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-05-16 11:16:44 +0100

    Added scope

--- pocket.py
+++ pocket.py
@@ -1,1 +1,17 @@
 #Simple example using lists
+"""Function with steps:
+'pocket' to be a list consisting of the strings 'seashell', 'strange berry', and 'lint'
+.sort() the items in the list stored under the 'backpack' key
+.remove('dagger') from the list of items stored under the 'backpack' key
+Add 50 to the number stored under the 'gold' key
+inventory = {
+  'gold' : 500,
+  'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+  'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+Add a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+"""
