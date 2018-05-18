31e3a9a6627153bff55b389e4d319e6c8f085a71
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-05-18 14:11:12 +0100

    Adding some function docstrings

--- class_average.py
+++ class_average.py
@@ -19,23 +19,46 @@ tyler = {
 }
 
 def average(numbers):
+  """Calculate average grade.
+  Args:
+    numbers (float): grade number
+  Returns:
+    average_grade (float): the average of grades for each subject
+  """
   total = sum(numbers)
   total = float(total)
-  return total/len(numbers)
+  average_grade = total/len(numbers)
+  
+  return average_grade
 
 def get_average(student):
+  """Calculate student average.
+  Args:
+    student ():
+  Returns:
+   student_average ():
+  """
   homework = average(student["homework"])
   quizzes = average(student["quizzes"])
   tests = average(student["tests"])
-  return 0.1 * homework + 0.3 * quizzes + 0.6 * tests
+  student_average = 0.1 * homework + 0.3 * quizzes + 0.6 * tests
+  
+  return student_average
 
 def get_class_average(class_list):
+  """Calculate class average.
+  Args:
+    class_list (list):
+  Returns:
+    student_avg ():
+  """
   results = []
   for student in class_list:
     student_avg = get_average(student)
     results.append(student_avg)
+    
   return average(results)
 
-students = [lloyd, alice, tyler]
 
+students = [lloyd, alice, tyler]
 print get_class_average(students)
