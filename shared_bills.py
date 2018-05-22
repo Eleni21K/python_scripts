c42e6c319445f7069fb7c363a046ee21ffcb21e8
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-05-22 09:48:57 +0100

    initial problem

---
+++ shared_bills.py
@@ -0,0 +1,11 @@
+# Bill calculation in a shared household
+# 3 tenants: E, K, S
+# Dict = { "bill", amount}
+
+def total_sum():
+
+def calculate_average():
+
+def individual_ammount():
+
+def difference_from_average():
