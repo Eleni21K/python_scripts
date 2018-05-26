e49afc5d3be3d38a893e43cc0af4f6ba4c580285
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-05-26 12:17:41 +0100

    Added most docstrings

--- shared_bills.py
+++ shared_bills.py
@@ -3,9 +3,36 @@
 # Dict = { "bill", amount}
 
 def total_sum():
+  """Calculate the sum of all bills.
+  Returns:
+    total_sum (float): the sum of the bills
+  """
+  
+  return total_sum
 
-def calculate_average():
+def calculate_average(total_sum, number):
+  """Calculate the sum that each should pay.
+  Args:
+    total_sum (float): total sum of the bills
+    number (int): number of people
+  Returns:
+     average_sum (float): average for each person
+  """
+  
+  return average_sum
 
 def individual_ammount():
+  """Calculate the amount each person paid.
+  Args:
+  Returns:
+  """
 
-def difference_from_average():
+def difference_from_average(average_sum):
+  """Calculate the difference from what they paid and what they should pay.
+  Args:
+    average_sum (float): total sum of the bills
+  Returns:
+    difference (float): difference for each person
+  """
+    
+  
