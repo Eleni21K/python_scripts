ab9518d2427fbc68f5abffb04679cc9df2d20e79
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-06-16 16:09:50 +0100

    Update generate_list_without_duplicates.py

--- generate_list_without_duplicates.py
+++ generate_list_without_duplicates.py
@@ -1,10 +1,7 @@
 '''
-Take two lists, say for example these two:
-
-	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes. Write this using at least one list comprehension. (Hint: Remember list comprehensions from Exercise 7).
-
+Take two lists and write a program that returns a list that contains only the elements
+that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+Write this using at least one list comprehension. 
 Extra:
 
 Randomly generate two lists to test this
