abd2718d4d5da845199c0caf6c2d57042ade4a3e
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-06-18 10:04:09 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,6 @@
 '''
-Write a program that asks the user how many Fibonnaci numbers 
-to generate and then generates them. Take this opportunity to
-think about how you can use functions. Make sure to ask the user
+Write a program that asks the user how many Fibonacci numbers 
+to generate and then generates them. Ask the user
 to enter the number of numbers in the sequence to generate.
 (Hint: The Fibonnaci seqence is a sequence of numbers where the 
 next number in the sequence is the sum of the previous two numbers
