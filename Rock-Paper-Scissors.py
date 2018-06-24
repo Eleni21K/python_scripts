d1aeb3a5771eb84eacfc0ec96a728110c27edeb2
Eleni <31955471+Eleni21K@users.noreply.github.com>
2018-06-24 22:24:21 +0100

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,4 +1,14 @@
+'''Make a two-player Rock-Paper-Scissors game. 
+(Hint: Ask for player plays (using input), compare them,
+ return out a message of congratulations to the winner, 
+ and ask if the players want to start a new game)
 
+Remember the rules:
+
+Rock beats scissors
+Scissors beats paper
+Paper beats rock
+'''
 
 import sys
 
